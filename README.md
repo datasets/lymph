59372be96d30c0041d3a408eb3a2ba43095cd965
svetozarstojkovic <svetozar.cvele.stojkovic@gmail.com>
2018-05-16 11:39:46 +0200

    [init][l]: * added data/lymph.csv; * scripts/main.py; * datapackage.json; * README.md

---
+++ README.md
@@ -0,0 +1,29 @@
+This dataset contains occurrences of lymph cancer in people
+
+## Data
+
+This dataset was found on [OpenML - lymph](https://www.openml.org/d/10)
+
+This lymphography domain was obtained from the University Medical Centre,
+Institute of Oncology, Ljubljana, Yugoslavia.  Thanks go to M. Zwitter and
+M. Soklic for providing the data. Please include this citation if you plan
+to use this database.
+
+Data is located in directory `data`
+
+`data/lymph.csv`
+
+## Preparation
+
+Data wrangling contains replacing yes and no values with true and false.
+
+Scripts are in directory `scripts`
+
+`scripts/main.py`
+
+## Licence
+Licensed under the [Public Domain Dedication and Licence][pddl] (assuming
+either no rights or public domain licence in source data).
+
+[pddl]: http://opendatacommons.org/licenses/pddl/1.0/
+
---
+++ scripts/main.py
@@ -0,0 +1,20 @@
+from urllib.request import urlopen
+
+with open("../data/lymph.csv", "w") as output_file:
+    data_found = False
+    attributes = []
+    for line in urlopen("https://www.openml.org/data/download/10/dataset_10_lymph.arff"):
+        decoded_line = line.decode('UTF-8').strip()
+        if '@attribute' in decoded_line:
+            apos = [pos for pos, char in enumerate(decoded_line) if char == "\'"]
+            attributes.append(decoded_line[apos[0]: apos[1]].replace('\'', ''))
+        if data_found:
+            decoded_line = decoded_line.replace('yes', 'true')
+            decoded_line = decoded_line.replace('no', 'false')
+            output_file.write(decoded_line.lower() + '\n')
+
+        if decoded_line == "@data":
+            output_file.write(','.join(attributes).lower() + '\n')
+            data_found = True
+
+    output_file.close()
